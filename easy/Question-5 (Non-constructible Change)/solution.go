038a40758d1b2fc5dc6f02e419e0793898b1ef07
Ali Naqvi <syedalihamad@hotmail.com>
2023-12-13 22:59:46 +0500

    solved Question 5

--- easy/Question-5 (Non-constructible Change)/solution.go
+++ easy/Question-5 (Non-constructible Change)/solution.go
@@ -8,11 +8,11 @@ import (
 func LowestNonExistantChange(coins []int) int {
 	sort.Ints(coins)
 	hPC := 0
-	for _,v:=range coins {
-		if v>hPC+1{
-			return hPC+1
+	for _, v := range coins {
+		if v > hPC+1 {
+			return hPC + 1
 		} else {
-		hPC=hPC+v
+			hPC = hPC + v
 		}
 	}
 
@@ -22,5 +22,5 @@ func main() {
 	coins := []int{5, 7, 1, 1, 2, 3, 22}
 	L := LowestNonExistantChange(coins)
 
-	fmt.Println("lowest change not possible is:", L )
+	fmt.Println("lowest change not possible is:", L)
 }
